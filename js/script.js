3b59c665e2e50d61638773a7e6ff7af7992151ed
elahoda <elahoda@gmail.com>
2023-03-14 10:10:54 -0400

    Fetch user Data

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//div where my profile info goes
+const overview = document.querySelector(".overview");
+const username = "elahoda";
+const getProfile = async function() {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    
+    displayData(data);
+};
+getProfile();
+
+
+const displayData = function (data){
+const div = document.createElement("div");
+div.classList.add(".user-info");
+div.innerHTML = `<figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div> `;
+
+    overview.append(div);
+
+};
